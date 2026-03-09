f279f7c2cba383e8a76c5149ce3e71b2ceb95282
mdleion9 <mdleionmdleion50@gmail.com>
2026-03-09 21:49:59 +0600

    login form js compelete

--- login.js
+++ login.js
@@ -1,1 +1,24 @@
- 
+  document.getElementById("loginForm")
+.addEventListener("submit",function(e){
+
+e.preventDefault()
+
+const username =
+document.getElementById("username").value.trim()
+
+const password =
+document.getElementById("password").value.trim()
+
+if(username === "admin" && password === "admin123"){
+
+window.location.href = "home .html"
+
+}
+else{
+
+alert("Wrong username or password")
+
+}
+
+})
+
